13e48dac630c22659ef13a15718c29ae4de66b53
Martin Grignard <mar.grignard@gmail.com>
2023-01-05 09:32:16 +0100

    Update tissues definition

--- code/brainweb_tdcs/tissues.py
+++ code/brainweb_tdcs/tissues.py
@@ -28,9 +28,9 @@ class Tissue:
 
 
 TISSUES = {
-    "WM": Tissue("white matter", 0.0646, 0.81, 0.2167, 0.1703),
-    "GM": Tissue("Gray matter", 0.06, 2.47, 0.466, 0.2392),
-    "CSF": Tissue("Cerebrospinal fluid", 1.0, 2.51, 1.71, 0.2981),
-    "SKL": Tissue("Skull", 0.0182, 1.718, 0.016, 0.019),
-    "SFT": Tissue("Soft tissue", 0.137, 2.1, 0.4137, 0.176),
+    "WM": Tissue("white matter", 0.0646, 0.81, 0.2167, 0.1703, "#440154"),
+    "GM": Tissue("Gray matter", 0.06, 2.47, 0.466, 0.2392, "#3a528b"),
+    "CSF": Tissue("Cerebrospinal fluid", 1.0, 2.51, 1.71, 0.2981, "#20908c"),
+    "SKL": Tissue("Skull", 0.0182, 1.718, 0.016, 0.019, "#5ec961"),
+    "SFT": Tissue("Soft tissue", 0.137, 2.1, 0.4137, 0.176, "#fde724"),
 }
